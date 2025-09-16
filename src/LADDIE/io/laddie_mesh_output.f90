0dee4d6801aa9fde6c205e32191d0b7a8b310a92
Erwin Lambert <45197956+erwinlambert@users.noreply.github.com>
2025-09-16 11:55:41 +0200

    add Hs and Hb to laddie forcing and output

--- src/LADDIE/io/laddie_mesh_output.f90
+++ src/LADDIE/io/laddie_mesh_output.f90
@@ -170,6 +170,10 @@ contains
       ! Ice geometry
       case ('Hi')
         call write_to_field_multopt_mesh_dp_2D_notime( mesh, laddie%output_mesh_filename, ncid, 'Hi', forcing%Hi, d_is_hybrid = .true.)
+      case ('Hs')
+        call write_to_field_multopt_mesh_dp_2D_notime( mesh, laddie%output_mesh_filename, ncid, 'Hs', forcing%Hs, d_is_hybrid = .true.)
+      case ('Hb')
+        call write_to_field_multopt_mesh_dp_2D_notime( mesh, laddie%output_mesh_filename, ncid, 'Hb', forcing%Hb, d_is_hybrid = .true.)
       case ('Hib')
         call write_to_field_multopt_mesh_dp_2D_notime( mesh, laddie%output_mesh_filename, ncid, 'Hib', forcing%Hib, d_is_hybrid = .true.)
       case ('TAF')
@@ -424,6 +428,10 @@ contains
       ! Ice geometry
       case ('Hi')
         call add_field_mesh_dp_2D_notime( filename, ncid, 'Hi', long_name = 'Ice thickness', units = 'm')
+      case ('Hs')
+        call add_field_mesh_dp_2D_notime( filename, ncid, 'Hs', long_name = 'Ice surface elevation', units = 'm')
+      case ('Hb')
+        call add_field_mesh_dp_2D_notime( filename, ncid, 'Hb', long_name = 'Bedrock elevation', units = 'm')
       case ('Hib')
         call add_field_mesh_dp_2D_notime( filename, ncid, 'Hib', long_name = 'Ice base elevation', units = 'm w.r.t. sea level')
       case ('TAF')

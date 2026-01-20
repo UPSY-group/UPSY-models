4496d178f310096f1c671702d2ee2e02c8e5e9e5
TijnBerends <c.j.berends@uu.nl>
2026-01-20 19:39:37 +0100

    Add atype_model_context to UPSY_main
    Hurray

--- src/UPSY/main/UPSY_main.f90
+++ src/UPSY/main/UPSY_main.f90
@@ -1,6 +1,6 @@
 module UPSY_main
   ! The main UPSY module that should be imported into models built with it.
 
-use models_basic, only: atype_model
+use models_basic, only: atype_model, atype_model_context
 
 end module UPSY_main

9eef816a64aabe6750cfca5c62dcf1ad95581da0
TijnBerends <c.j.berends@uu.nl>
2026-04-21 09:56:06 +0200

    Or this?
    Barp

--- src/UPSY/mesh/graph/mesh_graph_mapping.f90
+++ src/UPSY/mesh/graph/mesh_graph_mapping.f90
@@ -931,8 +931,7 @@ contains
     ! In/output variables:
     type(type_graph),                 intent(in   ) :: graph
     real(dp), dimension(:,:), target, intent(  out) :: d_mesh
-    real(dp), dimension(graph%pai%i1_nih:graph%pai%i2_nih, &
-      1:size(d_mesh,2)),      target, intent(in   ) :: d_graph_nih
+    real(dp), dimension(:,:), target, intent(in   ) :: d_graph_nih
     type(type_mesh),                  intent(in   ) :: mesh
 
     ! Local variables:
@@ -944,9 +943,8 @@ contains
     call init_routine( routine_name)
 
     do k = 1, size( d_mesh,2)
-      ! Explicitly preserve first-dimension bounds when slicing to 2D kernels.
-      d_mesh_k( lbound( d_mesh,1): ubound( d_mesh,1)) => d_mesh( :,k)
-      d_graph_nih_k( lbound( d_graph_nih,1): ubound( d_graph_nih,1)) => d_graph_nih( :,k)
+      d_mesh_k      => d_mesh     ( :,k)
+      d_graph_nih_k => d_graph_nih( :,k)
       call map_graph_to_mesh_vertices_dp_2D( graph, d_graph_nih_k, mesh, d_mesh_k)
     end do
 
@@ -1163,8 +1161,7 @@ contains
     ! In/output variables:
     type(type_graph),                 intent(in   ) :: graph
     real(dp), dimension(:,:), target, intent(  out) :: d_mesh
-    real(dp), dimension(graph%pai%i1_nih:graph%pai%i2_nih, &
-      1:size(d_mesh,2)),      target, intent(in   ) :: d_graph_nih
+    real(dp), dimension(:,:), target, intent(in   ) :: d_graph_nih
     type(type_mesh),                  intent(in   ) :: mesh
 
     ! Local variables:
@@ -1176,9 +1173,8 @@ contains
     call init_routine( routine_name)
 
     do k = 1, size( d_mesh,2)
-      ! Explicitly preserve first-dimension bounds when slicing to 2D kernels.
-      d_mesh_k( lbound( d_mesh,1): ubound( d_mesh,1)) => d_mesh( :,k)
-      d_graph_nih_k( lbound( d_graph_nih,1): ubound( d_graph_nih,1)) => d_graph_nih( :,k)
+      d_mesh_k      => d_mesh     ( :,k)
+      d_graph_nih_k => d_graph_nih( :,k)
       call map_graph_to_mesh_triangles_dp_2D( graph, d_graph_nih_k, mesh, d_mesh_k)
     end do
 

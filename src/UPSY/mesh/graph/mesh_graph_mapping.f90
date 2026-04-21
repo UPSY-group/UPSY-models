f0f1fcc438cc4bc31bdd465f1b01d566cd34e066
TijnBerends <c.j.berends@uu.nl>
2026-04-21 09:21:22 +0200

    Will this work?
    HMMM...

--- src/UPSY/mesh/graph/mesh_graph_mapping.f90
+++ src/UPSY/mesh/graph/mesh_graph_mapping.f90
@@ -944,8 +944,9 @@ contains
     call init_routine( routine_name)
 
     do k = 1, size( d_mesh,2)
-      d_mesh_k      => d_mesh     ( :,k)
-      d_graph_nih_k => d_graph_nih( :,k)
+      ! Explicitly preserve first-dimension bounds when slicing to 2D kernels.
+      d_mesh_k( lbound( d_mesh,1): ubound( d_mesh,1)) => d_mesh( :,k)
+      d_graph_nih_k( lbound( d_graph_nih,1): ubound( d_graph_nih,1)) => d_graph_nih( :,k)
       call map_graph_to_mesh_vertices_dp_2D( graph, d_graph_nih_k, mesh, d_mesh_k)
     end do
 
@@ -1175,8 +1176,9 @@ contains
     call init_routine( routine_name)
 
     do k = 1, size( d_mesh,2)
-      d_mesh_k      => d_mesh     ( :,k)
-      d_graph_nih_k => d_graph_nih( :,k)
+      ! Explicitly preserve first-dimension bounds when slicing to 2D kernels.
+      d_mesh_k( lbound( d_mesh,1): ubound( d_mesh,1)) => d_mesh( :,k)
+      d_graph_nih_k( lbound( d_graph_nih,1): ubound( d_graph_nih,1)) => d_graph_nih( :,k)
       call map_graph_to_mesh_triangles_dp_2D( graph, d_graph_nih_k, mesh, d_mesh_k)
     end do
 

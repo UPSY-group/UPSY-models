b120aec1ea8f5c1889849301923c1f5d79b1e013
TijnBerends <c.j.berends@uu.nl>
2026-04-27 13:24:59 +0200

    Add text file comparison script

---
+++ automated_testing/compare_all_texts_in_test_folder.m
@@ -0,0 +1,42 @@
+function compare_all_texts_in_test_folder( foldername)
+
+if ~exist( foldername,'dir')
+  error(['couldnt find test "' foldername '"'])
+end
+
+foldername_ref = [foldername '/reference'];
+if ~exist( foldername_ref,'dir')
+  error(['couldnt find reference for test "' foldername '"'])
+end
+foldername_mod = [foldername '/results'];
+if ~exist( foldername_mod,'dir')
+  error(['couldnt find results for test "' foldername '"'])
+end
+
+all_files_match = true;
+
+henk = dir( foldername_ref);
+for i = 1: length( henk)
+  filename_ref = [foldername_ref '/' henk(i).name];
+  filename_mod = [foldername_mod '/' henk(i).name];
+  if ~endsWith( filename_ref,'.txt'); continue; end
+
+  disp(['Comparing text file ' num2str(i) '/' num2str( length( henk)) ': ' filename_ref])
+
+  if ~exist( filename_mod,'file')
+    error(['file "' henk(i).name '" does not exist in results of test "' foldername '"'])
+  end
+
+  files_match = compare_text_file( filename_ref, filename_mod);
+
+  if ~files_match
+    all_files_match = false;
+  end
+
+end
+
+if ~all_files_match
+  error('Not all files are identical')
+end
+
+end

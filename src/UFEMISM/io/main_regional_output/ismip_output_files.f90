fc07723cf8bf87226cc84f791ee2a431b5614542
erwinlambert <erwin.lambert@knmi.nl>
2026-05-08 15:46:58 +0200

    fix TAF

--- src/UFEMISM/io/main_regional_output/ismip_output_files.f90
+++ src/UFEMISM/io/main_regional_output/ismip_output_files.f90
@@ -668,7 +668,9 @@ contains
       case ('limnsw')
         scalar_loc = 0._dp
         do vi = region%mesh%vi1, region%mesh%vi2
-          scalar_loc = scalar_loc + region%ice%TAF( vi) * region%mesh%A( vi) * ice_density
+          if (region%ice%Hi( vi) > 0._dp) then
+            scalar_loc = scalar_loc + max(0._dp,region%ice%TAF( vi)) * region%mesh%A( vi) * ice_density
+          end if
         end do
         call MPI_ALLREDUCE( MPI_IN_PLACE, scalar_loc, 1, MPI_DOUBLE_PRECISION, MPI_SUM, MPI_COMM_WORLD, ierr)
         call write_to_field_multopt_dp_0D( scalar%filename, ncid, scalar%name, scalar_loc)

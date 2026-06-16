2ce7f71d4dc1251d8ba5e25e5a5867e35bde172e
Erwin Lambert <45197956+erwinlambert@users.noreply.github.com>
2026-06-16 13:23:45 +0200

    Bunch of SMB improvements (#655)
    * add SMB offset to ISMIP7
    
    * add flag whether to read and apply offset
    
    * fix type namelist of SMB_offset choice
    
    * add mwe to mie conversion in prescribed SMB
    
    * fix strbasemag output
    
    * add min 0 to hfgeoubed
    
    * allow min max in flux output
    
    * update references
    
    * fix monthly ISMIP SMB
    
    * revert monthly conversion of acabf, acabf-anom
    
    * correct units in SMB prescribed unit test
    
    * update refs
    
    * move division by 12

--- src/UFEMISM/ISMIP7_forcing/ISMIP7_SMB.f90
+++ src/UFEMISM/ISMIP7_forcing/ISMIP7_SMB.f90
@@ -64,8 +64,9 @@ module ISMIP7_SMB
 
       ! Baseline SMB and surface elevation
       real(dp), dimension(:), contiguous, pointer   :: SMB_baseline  => null()   !< [m.i.e. yr^-1]           Baseline yearly total SMB
+      real(dp), dimension(:), contiguous, pointer   :: SMB_offset    => null()   !< [m.i.e. yr^-1]           Offset in SMB to shift baseline period to 1960-1989
       real(dp), dimension(:), contiguous, pointer   :: Hs_baseline   => null()   !< [m w.r.t. PD sea level]  Baseline surface elevation
-      type(MPI_WIN) :: wSMB_baseline, wHs_baseline
+      type(MPI_WIN) :: wSMB_baseline, wHs_baseline, wSMB_offset
 
       ! ISMIP7-style input forcing fields
       type(type_ISMIP7_forcing_field_monthly)       :: acabf                     !< [m.i.e. month^_1]        Monthly total SMB          (scaled from SI units to ice model units upon reading)
@@ -227,6 +228,16 @@ contains
         units     = 'm.i.e. yr^-1', &
         remap_method = 'reallocate')
 
+      if (C%SMB_ISMIP7_choice_SMB_offset) then
+        ! SMB offset
+        call self%create_field( self%SMB_offset, self%wSMB_offset, &
+          self%mesh, Arakawa_grid%a(), &
+          name      = 'SMB_offset', &
+          long_name = 'Offset to shift SMB baseline period', &
+          units     = 'm.i.e. yr^-1', &
+          remap_method = 'reallocate')
+      end if
+
       ! Allocate anomalies (as ISMIP7 forcing fields)
       call self%acabf_anomaly%allocate( self, 'acabf-anomaly', 'Monthly total SMB anomaly', 'm.i.e. month^-1')
 
@@ -341,8 +352,24 @@ contains
     call read_field_from_file_2D( trim( C%SMB_ISMIP7_filename_SMB_baseline_fixed), 'SMB||surface_mass_balance', &
       mesh, C%output_dir, self%SMB_baseline)
 
+    if (C%SMB_ISMIP7_choice_SMB_offset) then
+      if (par%primary) then
+        write(0,*) '   Reading SMB offset from file: ', &
+          UPSY%stru%colour_string( trim( C%SMB_ISMIP7_filename_SMB_offset), 'light blue')
+      end if
+
+      call read_field_from_file_2D( trim( C%SMB_ISMIP7_filename_SMB_offset), &
+        'SMB||surface_mass_balance', mesh, C%output_dir, self%SMB_offset)
+
+      ! Subtract offset from SMB_baseline
+      self%SMB_baseline( mesh%vi1:mesh%vi2) = self%SMB_baseline( mesh%vi1:mesh%vi2) &
+        - self%SMB_offset( mesh%vi1:mesh%vi2)
+
+    end if
+
     ! Convert from [m.w.e. yr^-1] to [m.i.e. yr^-1]
-    self%SMB_baseline( mesh%vi1:mesh%vi2) = self%SMB_baseline( mesh%vi1:mesh%vi2) * freshwater_density / ice_density
+    self%SMB_baseline( mesh%vi1:mesh%vi2) = self%SMB_baseline( mesh%vi1:mesh%vi2) &
+      * freshwater_density / ice_density
 
     ! Finalise routine path
     call finalise_routine( routine_name)
@@ -385,7 +412,8 @@ contains
       ! Calculate monthly SMB
       do vi = mesh%vi1, mesh%vi2
         do mi = 1, 12
-          self%SMB_monthly( vi, mi) = self%acabf%val_interp( vi, mi) + self%delta_SMB( vi)
+                                      ! Divide delta by 12 to convert from [m.i.e. yr^-1] to [m.i.e. month^-1]
+          self%SMB_monthly( vi, mi) = self%acabf%val_interp( vi, mi) + self%delta_SMB( vi) / 12._dp
         end do
       end do
 
@@ -397,14 +425,14 @@ contains
       ! Calculate monthly climate
       do vi = mesh%vi1, mesh%vi2
         do mi = 1, 12
-                                      ! Divide baseline by 12 to convert from [m.i.e. yr^-1] to [m.i.e. month^-1]
-          self%SMB_monthly( vi, mi) = self%SMB_baseline( vi) / 12._dp + self%acabf_anomaly%val_interp( vi, mi) + self%delta_SMB( vi)
+                                      ! Divide baseline and delta by 12 to convert from [m.i.e. yr^-1] to [m.i.e. month^-1]
+          self%SMB_monthly( vi, mi) = (self%SMB_baseline( vi) + self%delta_SMB( vi)) / 12._dp + self%acabf_anomaly%val_interp( vi, mi)
         end do
       end do
 
     end select
 
-    ! Calculate yearly SMB
+    ! Calculate yearly SMB by summing all monthly values
     do vi = mesh%vi1, mesh%vi2
       self%SMB( vi) = sum( self%SMB_monthly( vi,:))
     end do

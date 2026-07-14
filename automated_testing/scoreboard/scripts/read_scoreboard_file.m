256d8d5a81e4a696e3281e7d9ac0e17857528bc7
TijnBerends <c.j.berends@uu.nl>
2026-07-14 15:36:11 +0200

    Delete obsolete test suite stuff
    Spring cleaning!

--- automated_testing/scoreboard/scripts/read_scoreboard_file.m
+++
@@ -1,7 +0,0 @@
-function single_run = read_scoreboard_file( filename)
-% Read a structure containing the results of all the runs of a
-% component/integrated test from a scoreboard file
-
-single_run = readstruct( filename);
-
-end

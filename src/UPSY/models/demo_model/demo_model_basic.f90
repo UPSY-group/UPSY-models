a3a515c5b94fb668af180875324cc550c506afcb
TijnBerends <c.j.berends@uu.nl>
2026-07-19 19:32:27 +0200

    Update remap abstract demo model

--- src/UPSY/models/demo_model/demo_model_basic.f90
+++ src/UPSY/models/demo_model/demo_model_basic.f90
@@ -6,7 +6,6 @@ module demo_model_basic
   use mesh_types, only: type_mesh
   use Arakawa_grid_mod, only: Arakawa_grid
   use fields_main, only: third_dimension
-  use models_basic, only: atype_model_context_remap
   use demo_model_data, only: atype_demo_model_data
   use mpi_f08, only: MPI_WIN
 
@@ -14,7 +13,7 @@ module demo_model_basic
 
   private
 
-  public :: atype_demo_model, type_demo_model_context_remap
+  public :: atype_demo_model
 
   type, abstract, extends(atype_demo_model_data) :: atype_demo_model
     !< Stuff that is common to all demo models
@@ -24,33 +23,21 @@ module demo_model_basic
 
     contains
 
-      ! These routines all consist of two parts: a 'common' part that is executed for
-      ! all models inheriting from atype_demo_model, and a 'specific' part that is
-      ! only executed for each specific model class. The specific parts are defined
-      ! in the deferred procedures 'allocate_demo_model', 'initialise_demo_model', etc.
-
+      ! Type-bound procedures that apply to all demo models
       procedure, public :: allocate_demo_model
       procedure, public :: deallocate_demo_model
       procedure, public :: initialise_demo_model
       procedure, public :: run_demo_model
-      procedure, public :: remap_model      => remap_model_abs
+      procedure, public :: remap_demo_model
 
+      ! Deferred procedures that must be defined by each individual demo model
       procedure(demo_model_allocate_ifc),   deferred :: allocate
       procedure(demo_model_deallocate_ifc), deferred :: deallocate
       procedure(demo_model_initialise_ifc), deferred :: initialise
       procedure(demo_model_run_ifc),        deferred :: run
-      procedure(remap_demo_model_ifc),      deferred :: remap_demo_model
-
-      ! Factory functions to create model context objects
-      procedure, nopass, public :: ct_remap
+      procedure(demo_model_remap_ifc),      deferred :: remap
 
   end type atype_demo_model
-
-  ! Context classes for allocate/initialise/run/remap
-  ! =================================================
-
-  type, extends(atype_model_context_remap) :: type_demo_model_context_remap
-  end type type_demo_model_context_remap
 
   ! Abstract interfaces for deferred procedures
   ! ===========================================
@@ -85,28 +72,11 @@ module demo_model_basic
       real(dp),                intent(in   ) :: dH
     end subroutine demo_model_run_ifc
 
-    subroutine remap_demo_model_ifc( self, context)
-      import atype_demo_model, type_demo_model_context_remap
-      class(atype_demo_model),                     intent(inout) :: self
-      type(type_demo_model_context_remap), target, intent(in   ) :: context
-    end subroutine remap_demo_model_ifc
-
-  end interface
-
-  ! Interfaces to type-bound procedures defined in submodules
-  ! =========================================================
-
-  interface
-
-    module subroutine remap_model_abs( self, context)
-      class(atype_demo_model),                  intent(inout) :: self
-      class(atype_model_context_remap), target, intent(in   ) :: context
-    end subroutine remap_model_abs
-
-    module function ct_remap( mesh_new) result( context)
-      type(type_mesh), target, intent(in) :: mesh_new
-      type(type_demo_model_context_remap) :: context
-    end function ct_remap
+    subroutine demo_model_remap_ifc( self, mesh_new)
+      import atype_demo_model, type_mesh
+      class(atype_demo_model), intent(inout) :: self
+      type(type_mesh), target, intent(in   ) :: mesh_new
+    end subroutine demo_model_remap_ifc
 
   end interface
 
@@ -280,5 +250,35 @@ contains
     call finalise_routine( routine_name)
 
   end subroutine run_demo_model
+
+  subroutine remap_demo_model( self, mesh_new)
+    !< Remap stuff that is common to all demo models
+    !< (call this from your demo model-specific remap routine)
+
+    ! In/output variables:
+    class(atype_demo_model), intent(inout) :: self
+    type(type_mesh), target, intent(in   ) :: mesh_new
+
+    ! Local variables:
+    character(len=*), parameter :: routine_name = 'remap_demo_model'
+
+    ! Add routine to call stack
+    call init_routine( routine_name)
+
+    ! Remap stuff that is common to all models
+    call self%remap_model( mesh_new)
+
+    ! Remap stuff that is specific to demo models
+
+    call self%remap_field( mesh_new, 'H'       , self%H       )
+    call self%remap_field( mesh_new, 'u_3D'    , self%u_3D    )
+    call self%remap_field( mesh_new, 'v_3D'    , self%v_3D    )
+    call self%remap_field( mesh_new, 'mask_ice', self%mask_ice)
+    call self%remap_field( mesh_new, 'T2m'     , self%T2m     )
+
+    ! Remove routine from call stack
+    call finalise_routine( routine_name)
+
+  end subroutine remap_demo_model
 
 end module demo_model_basic

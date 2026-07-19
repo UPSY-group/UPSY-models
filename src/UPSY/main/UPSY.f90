9736867d5b14ab6d5144f6e860fcd847257ce106
TijnBerends <c.j.berends@uu.nl>
2026-07-19 19:22:41 +0200

    Remove remap deferred procedure from atype_model

--- src/UPSY/main/UPSY.f90
+++ src/UPSY/main/UPSY.f90
@@ -1,8 +1,7 @@
 module UPSY_main
   !< The main UPSY module that should be imported into models built with it.
 
-use models_basic, only: atype_model, &
-  atype_model_context_remap
+use models_basic, only: atype_model
 use string_module, only: type_string_utilities
 
 implicit none
@@ -10,8 +9,7 @@ implicit none
 private
 
 public :: UPSY
-public :: atype_model, &
-  atype_model_context_remap
+public :: atype_model
 
 type type_UPSY
     !< The complete UPSY toolkit

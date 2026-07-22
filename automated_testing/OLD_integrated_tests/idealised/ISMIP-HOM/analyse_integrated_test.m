72790de6274ef195a4e7f811cd9f20460f41d08e
TijnBerends <c.j.berends@uu.nl>
2026-07-22 15:28:52 +0200

    Remove old integrated tests directory
    Hurray

--- automated_testing/OLD_integrated_tests/idealised/ISMIP-HOM/analyse_integrated_test.m
+++
@@ -1,9 +0,0 @@
-function analyse_integrated_test( varargin)
-% Analyse the results of the ISMIP-HOM integrated tests
-
-analyse_integrated_test_ISMIP_HOM_A( varargin);
-analyse_integrated_test_ISMIP_HOM_B( varargin);
-analyse_integrated_test_ISMIP_HOM_C( varargin);
-analyse_integrated_test_ISMIP_HOM_D( varargin);
-
-end

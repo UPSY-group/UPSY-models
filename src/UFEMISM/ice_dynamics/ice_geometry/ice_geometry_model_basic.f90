b8fef7a104e2d36cf25bb77ced79dc3eca2180e2
TijnBerends <c.j.berends@uu.nl>
2026-08-09 12:55:06 +0200

    Convert Hib to hybrid memory

--- src/UFEMISM/ice_dynamics/ice_geometry/ice_geometry_model_basic.f90
+++ src/UFEMISM/ice_dynamics/ice_geometry/ice_geometry_model_basic.f90
@@ -212,20 +212,26 @@ contains
 
     ! Derived ice geometry variables
     allocate( self%Hs      ( mesh%vi1:mesh%vi2), source = NaN)
-    allocate( self%Hib     ( mesh%vi1:mesh%vi2), source = NaN)
+
+    call self%create_field( self%Hib, self%wHib, &
+      self%mesh, Arakawa_grid%a(), &
+      name      = 'Hib', &
+      long_name = 'Ice base elevation', &
+      units     = 'm w.r.t. PD sea level', &
+      remap_method = 'reallocate')
 
     call self%create_field( self%TAF, self%wTAF, &
       self%mesh, Arakawa_grid%a(), &
       name      = 'TAF', &
       long_name = 'Ice thickness above floatation', &
-      units     = '-', &
+      units     = 'm', &
       remap_method = 'reallocate')
 
     call self%create_field( self%Hi_eff, self%wHi_eff, &
       self%mesh, Arakawa_grid%a(), &
       name      = 'Hi_eff', &
       long_name = 'Effective ice thickness', &
-      units     = '-', &
+      units     = 'm', &
       remap_method = 'reallocate')
 
     call self%create_field( self%Hs_slope, self%wHs_slope, &
@@ -373,7 +379,7 @@ contains
       remap_method = 'reallocate')
 
     ! Remove routine from call stack
-    call finalise_routine( routine_name, n_extra_MPI_windows_expected = 22)
+    call finalise_routine( routine_name, n_extra_MPI_windows_expected = 23)
 
   end subroutine allocate_ice_geometry_model
 
@@ -400,7 +406,7 @@ contains
 
     ! Derived ice geometry variables
     deallocate( self%Hs      )
-    deallocate( self%Hib     )
+    nullify( self%Hib     )
     nullify( self%TAF     )
     nullify( self%Hi_eff  )
     nullify( self%Hs_slope)
--- src/UFEMISM/io/main_regional_output/ismip_output_files.f90
+++ src/UFEMISM/io/main_regional_output/ismip_output_files.f90
@@ -239,7 +239,8 @@ contains
     call write_to_file( region, region%ismip_output%lithk, inputfield_a=region%ice%geom%Hi, vmin=0._dp)
     call write_to_file( region, region%ismip_output%orog,  inputfield_a=region%ice%geom%Hs, vmin=0._dp)
     call write_to_file( region, region%ismip_output%topg,  inputfield_a=region%ice%geom%Hb)
-    call write_to_file( region, region%ismip_output%base,  inputfield_a=region%ice%geom%Hib)
+    d_loc_dp = region%ice%geom%Hib( region%mesh%vi1:region%mesh%vi2)
+    call write_to_file( region, region%ismip_output%base,  inputfield_a=d_loc_dp)
 
     ! Geothermal heat flux
     call write_to_file_grid_FL( region, region%ismip_output%hfgeoubed, vmin=0._dp)

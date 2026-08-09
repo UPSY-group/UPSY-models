f70bd03f0fdb79d33c83eb47f587b6ba94a097c3
TijnBerends <c.j.berends@uu.nl>
2026-08-09 09:16:18 +0200

    wut

--- src/UFEMISM/transects/remapping_transects.f90
+++ src/UFEMISM/transects/remapping_transects.f90
@@ -87,6 +87,7 @@ contains
     if (size( d_mesh,1) == mesh%pai_V%n_loc) then
       allocate( d_mesh_dist( mesh%vi1:mesh%vi2), source = d_mesh)
     elseif (size( d_mesh,1) == mesh%pai_V%n_nih) then
+      call crash('wut')
       allocate( d_mesh_dist( mesh%vi1:mesh%vi2), source = 0._dp)
       call hybrid_to_dist( mesh%pai_V, d_mesh, d_mesh_dist)
     else
@@ -155,6 +156,7 @@ contains
     if (size( d_mesh,1) == mesh%pai_V%n_loc) then
       allocate( d_mesh_dist( mesh%vi1:mesh%vi2, 1:size( d_mesh,2)), source = d_mesh)
     elseif (size( d_mesh,1) == mesh%pai_V%n_nih) then
+      call crash('wut')
       allocate( d_mesh_dist( mesh%vi1:mesh%vi2, 1:size( d_mesh,2)), source = 0._dp)
       call hybrid_to_dist( mesh%pai_V, size( d_mesh,2), d_mesh, d_mesh_dist)
     else
@@ -223,6 +225,7 @@ contains
     if (size( d_mesh,1) == mesh%pai_Tri%n_loc) then
       allocate( d_mesh_dist( mesh%ti1:mesh%ti2), source = d_mesh)
     elseif (size( d_mesh,1) == mesh%pai_Tri%n_nih) then
+      call crash('wut')
       allocate( d_mesh_dist( mesh%ti1:mesh%ti2), source = 0._dp)
       call hybrid_to_dist( mesh%pai_Tri, d_mesh, d_mesh_dist)
     else
@@ -291,6 +294,7 @@ contains
     if (size( d_mesh,1) == mesh%pai_Tri%n_loc) then
       allocate( d_mesh_dist( mesh%ti1:mesh%ti2, 1:size( d_mesh,2)), source = d_mesh)
     elseif (size( d_mesh,1) == mesh%pai_Tri%n_nih) then
+      call crash('wut')
       allocate( d_mesh_dist( mesh%ti1:mesh%ti2, 1:size( d_mesh,2)), source = 0._dp)
       call hybrid_to_dist( mesh%pai_Tri, size( d_mesh,2), d_mesh, d_mesh_dist)
     else

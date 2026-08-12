798b4cf95ccb23e0f30b9cbd6f4cd4ef617bd58e
TijnBerends <c.j.berends@uu.nl>
2026-08-12 20:57:00 +0200

    Add vertex connectivity as third dimension for fields

--- src/UPSY/fields/fields_dimensions.f90
+++ src/UPSY/fields/fields_dimensions.f90
@@ -18,10 +18,11 @@ module fields_dimensions
 
   type type_third_dimension_factory
   contains
-    procedure :: ice_zeta    => third_dimension_factory_ice_zeta
-    procedure :: month       => third_dimension_factory_month
-    procedure :: ocean_depth => third_dimension_factory_ocean_depth
-    procedure :: bedrock_CDF => third_dimension_factory_bedrock_CDF
+    procedure :: ice_zeta            => third_dimension_factory_ice_zeta
+    procedure :: month               => third_dimension_factory_month
+    procedure :: ocean_depth         => third_dimension_factory_ocean_depth
+    procedure :: bedrock_CDF         => third_dimension_factory_bedrock_CDF
+    procedure :: vertex_connectivity => third_dimension_factory_vertex_connectivity
   end type type_third_dimension_factory
 
   type type_third_dimension
@@ -110,6 +111,16 @@ contains
     allocate( res%val( res%n), source = NaN)
     res%units = '-'
   end function third_dimension_factory_bedrock_CDF
+
+  function third_dimension_factory_vertex_connectivity( self, n) result( res)
+    class(type_third_dimension_factory), intent(in) :: self
+    integer,                             intent(in) :: n
+    type(type_third_dimension)                      :: res
+    res%name = 'nC_mem'
+    res%n    = n
+    allocate( res%val( res%n), source = NaN)
+    res%units = '-'
+  end function third_dimension_factory_vertex_connectivity
 
   pure function test_third_dimension_equality( dim1, dim2) result( res)
     class(type_third_dimension), intent(in) :: dim1

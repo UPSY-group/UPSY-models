890807a82d38c553422ac88d06df9975a6465764
TijnBerends <c.j.berends@uu.nl>
2026-08-18 19:40:42 +0200

    Fix output dirs

--- automated_testing/UFEMISM/integrated_test_ISMIP_HOM_small/all_ISMIP_HOM_config_files/AA_config_factory.m
+++ automated_testing/UFEMISM/integrated_test_ISMIP_HOM_small/all_ISMIP_HOM_config_files/AA_config_factory.m
@@ -135,7 +135,7 @@ end
 
 
 function single_line = fixed_output_dir_config( opts)
-  single_line = ['fixed_output_dir_config = "automated_testing/UFEMISM/integrated_test_ISMIP_HOM_full/' ...
+  single_line = ['fixed_output_dir_config = "automated_testing/UFEMISM/integrated_test_ISMIP_HOM_small/' ...
     'results_ISMIP_HOM_' opts.experiment '_' opts.length_scale '_' opts.Stokes_approx '"'];
 end
 

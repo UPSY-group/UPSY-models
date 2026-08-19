0e8dd304189a0b82bb2c98475d07be72461254b6
TijnBerends <c.j.berends@uu.nl>
2026-08-19 13:57:18 +0200

    Provide 3D prescibed velocity BCs to all solvers

--- src/UFEMISM/ice_dynamics/momentum_balance/momentum_balance_solvers/momentum_balance_solver_SIASSA.f90
+++ src/UFEMISM/ice_dynamics/momentum_balance/momentum_balance_solvers/momentum_balance_solver_SIASSA.f90
@@ -105,16 +105,19 @@ contains
   end subroutine momentum_balance_solver_SIASSA_initialise
 
   subroutine momentum_balance_solver_SIASSA_run( self, ice, geom, bed_roughness, &
-    BC_prescr_mask_b, BC_prescr_u_b, BC_prescr_v_b)
+    BC_prescr_mask_b, BC_prescr_u_b, BC_prescr_v_b, BC_prescr_mask_bk, BC_prescr_u_bk, BC_prescr_v_bk)
 
     ! In/output variables:
     class(type_momentum_balance_solver_SIASSA), intent(inout) :: self
     class(atype_ice_model_data),                intent(inout) :: ice
     class(atype_ice_geometry_model_data),       intent(in   ) :: geom
     type(type_bed_roughness_model),             intent(in   ) :: bed_roughness
-    integer,  dimension(:), optional,           intent(in   ) :: BC_prescr_mask_b      ! Mask of triangles where velocity is prescribed
-    real(dp), dimension(:), optional,           intent(in   ) :: BC_prescr_u_b         ! Prescribed velocities in the x-direction
-    real(dp), dimension(:), optional,           intent(in   ) :: BC_prescr_v_b         ! Prescribed velocities in the y-direction
+    integer,  dimension(:  ), optional,         intent(in   ) :: BC_prescr_mask_b      ! Mask of triangles where velocity is prescribed
+    real(dp), dimension(:  ), optional,         intent(in   ) :: BC_prescr_u_b         ! Prescribed velocities in the x-direction
+    real(dp), dimension(:  ), optional,         intent(in   ) :: BC_prescr_v_b         ! Prescribed velocities in the y-direction
+    integer,  dimension(:,:), optional,         intent(in   ) :: BC_prescr_mask_bk     ! Mask of triangles where velocity is prescribed
+    real(dp), dimension(:,:), optional,         intent(in   ) :: BC_prescr_u_bk        ! Prescribed velocities in the x-direction
+    real(dp), dimension(:,:), optional,         intent(in   ) :: BC_prescr_v_bk        ! Prescribed velocities in the y-direction
 
     ! Local variables:
     character(len=*), parameter :: routine_name = 'run_momentum_balance_solver_SIASSA'
@@ -124,9 +127,9 @@ contains
 
     ! Run all the stuff that is specific to the SIA/SSA momentum balance solver
     call self%solver_SIA%run( ice, geom, bed_roughness, &
-      BC_prescr_mask_b, BC_prescr_u_b, BC_prescr_v_b)
+      BC_prescr_mask_b, BC_prescr_u_b, BC_prescr_v_b, BC_prescr_mask_bk, BC_prescr_u_bk, BC_prescr_v_bk)
     call self%solver_SSA%run( ice, geom, bed_roughness, &
-      BC_prescr_mask_b, BC_prescr_u_b, BC_prescr_v_b)
+      BC_prescr_mask_b, BC_prescr_u_b, BC_prescr_v_b, BC_prescr_mask_bk, BC_prescr_u_bk, BC_prescr_v_bk)
 
     self%n_visc_its = self%solver_SSA%n_visc_its
     self%n_Axb_its  = self%solver_SSA%n_Axb_its

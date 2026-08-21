5d203aab6975176aea8b10879751f6ef8ca7f72f
TijnBerends <c.j.berends@uu.nl>
2026-08-21 20:34:28 +0200

    Make calc_secondary_velocities type-bound to velocity model

--- src/UFEMISM/ice_dynamics/ice_velocities/ice_velocities_main.f90
+++ src/UFEMISM/ice_dynamics/ice_velocities/ice_velocities_main.f90
@@ -11,10 +11,6 @@ module ice_velocities_main
   use parameters, only: ice_density, seawater_density, pi
   use mesh_types, only: type_mesh
   use ice_model_data, only: atype_ice_model_data
-  use mesh_disc_apply_operators, only: ddx_a_a_2D, ddy_a_a_2D, map_b_a_2D, map_b_a_3D
-  use mpi_distributed_memory, only: gather_to_all
-  use mesh_zeta, only: vertical_average
-  use map_velocities_to_c_grid
   use vertical_velocities, only: calc_vertical_velocities
   use bed_roughness_model_types, only: type_bed_roughness_model
   use ice_geometry_model_data, only: atype_ice_geometry_model_data
@@ -64,127 +60,13 @@ contains
 
     call momentum_balance_solver%run( ice, geom, bed_roughness, vel, &
       BC_prescr_mask_b, BC_prescr_u_b, BC_prescr_v_b, BC_prescr_mask_bk, BC_prescr_u_bk, BC_prescr_v_bk)
-    call calc_secondary_velocities( mesh, vel)
+    call vel%calc_secondary_velocities()
     call calc_vertical_velocities( vel, mesh, ice, geom, BMB)
 
     ! Finalise routine path
     call finalise_routine( routine_name)
 
   end subroutine solve_stress_balance
-
-  subroutine calc_secondary_velocities( mesh, vel)
-    !< Calculate all secondary ice velocities (surface, base, vertical average)
-
-    ! In/output variables:
-    type(type_mesh),                      intent(in   ) :: mesh
-    class(atype_ice_velocity_model_data), intent(inout) :: vel
-
-    ! Local variables:
-    character(len=1024), parameter    :: routine_name = 'calc_secondary_velocities'
-    integer                           :: vi,ti
-    real(dp), dimension(mesh%nz)      :: u_prof, v_prof
-
-    ! Add routine to path
-    call init_routine( routine_name)
-
-    do ti = mesh%ti1, mesh%ti2
-
-      ! Surface
-      vel%u_surf_b(    ti) = vel%u_3D_b( ti,1)
-      vel%v_surf_b(    ti) = vel%v_3D_b( ti,1)
-      vel%uabs_surf_b( ti) = SQRT( vel%u_surf_b( ti)**2 + vel%v_surf_b( ti)**2)
-
-      ! Base
-      vel%u_base_b(    ti) = vel%u_3D_b( ti,C%nz)
-      vel%v_base_b(    ti) = vel%v_3D_b( ti,C%nz)
-      vel%uabs_base_b( ti) = SQRT( vel%u_base_b( ti)**2 + vel%v_base_b( ti)**2)
-
-      ! Vertical average
-      u_prof = vel%u_3D_b( ti,:)
-      v_prof = vel%v_3D_b( ti,:)
-      vel%u_vav_b( ti) = vertical_average( mesh%zeta, u_prof)
-      vel%v_vav_b( ti) = vertical_average( mesh%zeta, v_prof)
-      vel%uabs_vav_b( ti) = SQRT( vel%u_vav_b( ti)**2 + vel%v_vav_b( ti)**2)
-
-    end do
-
-    ! == Calculate velocities on the a-grid (needed to calculate the vertical velocity w, and for writing to output)
-
-    ! 3-D
-    call map_b_a_3D( mesh, vel%u_3D_b  , vel%u_3D  )
-    call map_b_a_3D( mesh, vel%v_3D_b  , vel%v_3D  )
-
-    ! Surface
-    call map_b_a_2D( mesh, vel%u_surf_b, vel%u_surf)
-    call map_b_a_2D( mesh, vel%v_surf_b, vel%v_surf)
-
-    ! Base
-    call map_b_a_2D( mesh, vel%u_base_b, vel%u_base)
-    call map_b_a_2D( mesh, vel%v_base_b, vel%v_base)
-
-    ! Vertical average
-    call map_b_a_2D( mesh, vel%u_vav_b , vel%u_vav )
-    call map_b_a_2D( mesh, vel%v_vav_b , vel%v_vav )
-
-    ! Absolute
-    do vi = mesh%vi1, mesh%vi2
-      vel%uabs_surf( vi) = sqrt( vel%u_surf( vi)**2 + vel%v_surf( vi)**2)
-      vel%uabs_base( vi) = sqrt( vel%u_base( vi)**2 + vel%v_base( vi)**2)
-      vel%uabs_vav(  vi) = sqrt( vel%u_vav(  vi)**2 + vel%v_vav(  vi)**2)
-    end do
-
-    call calc_u_vav_perp( mesh, vel)
-
-    ! Slide/shear ratio
-    do vi = mesh%vi1, mesh%vi2
-      vel%R_shear( vi) = (vel%uabs_base( vi) + 0.1_dp) / (vel%uabs_surf( vi) + 0.1_dp)
-    end do
-
-    ! Finalise routine path
-    call finalise_routine( routine_name)
-
-  end subroutine calc_secondary_velocities
-
-  subroutine calc_u_vav_perp( mesh, vel)
-    !< Calculate the vertically averaged ice velocity component
-    !< perpendicular to the shared Voronoi cell boundaries
-
-    ! In/output variables:
-    type(type_mesh),                      intent(in   ) :: mesh
-    class(atype_ice_velocity_model_data), intent(inout) :: vel
-
-    ! Local variables:
-    character(len=*), parameter            :: routine_name = 'calc_u_vav_perp'
-    real(dp), dimension(mesh%ei1:mesh%ei2) :: u_vav_c, v_vav_c
-    real(dp), dimension(mesh%nE)           :: u_vav_c_tot, v_vav_c_tot
-    integer                                :: vi, ci, ei, vj
-
-    ! Add routine to path
-    call init_routine( routine_name)
-
-    ! Calculate vertically averaged ice velocities on the edges
-    call map_velocities_from_b_to_c_2D( mesh, vel%u_vav_b, vel%v_vav_b, u_vav_c, v_vav_c)
-    call gather_to_all( u_vav_c, u_vav_c_tot)
-    call gather_to_all( v_vav_c, v_vav_c_tot)
-
-    do vi = mesh%vi1, mesh%vi2
-      do ci = 1, mesh%nC( vi)
-
-        ! Connection ci from vertex vi leads through edge ei to vertex vj
-        ei = mesh%VE( vi,ci)
-
-        ! Calculate vertically averaged ice velocity component perpendicular to this shared Voronoi cell boundary section
-        vel%u_vav_perp( vi, ci) = &
-          u_vav_c_tot( ei) * mesh%D_x( vi, ci)/mesh%D( vi, ci) + &
-          v_vav_c_tot( ei) * mesh%D_y( vi, ci)/mesh%D( vi, ci)
-
-      end do
-    end do
-
-    ! Finalise routine path
-    call finalise_routine( routine_name)
-
-  end subroutine calc_u_vav_perp
 
   subroutine remap_velocity_solver( vel, momentum_balance_solver, mesh_old, mesh_new, ice)
     !< Remap the velocity solver for the chosen stress balance approximation
@@ -204,7 +86,7 @@ contains
 
     call momentum_balance_solver%remap( mesh_old, mesh_new)
     call momentum_balance_solver%set_velocities_to_solver_results( ice, vel)
-    call calc_secondary_velocities( mesh_new, vel)
+    call vel%calc_secondary_velocities()
 
     ! Finalise routine path
     call finalise_routine( routine_name)

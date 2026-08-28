bfbf12ec8632375b30fd2cc9968c348a8d386b64
TijnBerends <c.j.berends@uu.nl>
2026-08-28 09:52:30 +0200

    Clean up GIA model types module

--- src/UFEMISM/types/GIA_model_types.f90
+++ src/UFEMISM/types/GIA_model_types.f90
@@ -1,61 +1,49 @@
-MODULE GIA_model_types
+module GIA_model_types
 
-  ! The different data types used in the GIA modules
+  use precisions, only: dp
+  use grid_types, only: type_grid
 
-! ===== Preamble =====
-! ====================
+  implicit none
 
-  USE precisions                                             , ONLY: dp
-  USE grid_types                                             , ONLY: type_grid
+  private
 
-  IMPLICIT NONE
+  public :: type_GIA_model, type_ELRA_model
 
-! ===== Types =====
-! =================
-
-  TYPE type_GIA_model
+  type type_GIA_model
     ! The GIA model data structure.
 
     ! The GIA model grid
-    TYPE(type_grid)                         :: grid
+    type(type_grid)                     :: grid
 
     ! Main data fields
-    REAL(dp), DIMENSION(:    ), ALLOCATABLE :: relative_surface_load_mesh  ! [Pa] Surface load relative to the GIA-equilibrium reference geometry, on the UFEMISM mesh
-    REAL(dp), DIMENSION(:    ), ALLOCATABLE :: relative_surface_load_grid  ! [Pa] Surface load relative to the GIA-equilibrium reference geometry, on the UFEMISM mesh
-    
+    real(dp), dimension(:), allocatable :: relative_surface_load_mesh  ! [Pa] Surface load relative to the GIA-equilibrium reference geometry, on the UFEMISM mesh
+    real(dp), dimension(:), allocatable :: relative_surface_load_grid  ! [Pa] Surface load relative to the GIA-equilibrium reference geometry, on the UFEMISM mesh
+
     ! Sub-models
 
     ! Timestepping
-    REAL(dp)                                :: t_prev                      ! [yr] Time of the previous state
-    REAL(dp)                                :: t_next                      ! [yr] Time of the next state
-    REAL(dp), DIMENSION(:    ), ALLOCATABLE :: dHb_prev                    ! [m]  The previous state (bedrock deflection relative to the GIA-equilibrium reference geometry)
-    REAL(dp), DIMENSION(:    ), ALLOCATABLE :: dHb_next                    ! [m]  The next state     (idem)
-    
-  END TYPE type_GIA_model
-  
- ! ==== Type for ELRA model ==== 
- ! =============================
+    real(dp)                            :: t_prev                      ! [yr] Time of the previous state
+    real(dp)                            :: t_next                      ! [yr] Time of the next state
+    real(dp), dimension(:), allocatable :: dHb_prev                    ! [m]  The previous state (bedrock deflection relative to the GIA-equilibrium reference geometry)
+    real(dp), dimension(:), allocatable :: dHb_next                    ! [m]  The next state     (idem)
 
-  TYPE type_ELRA_model
+  end type type_GIA_model
+
+  type type_ELRA_model
     ! The ELRA model data structure.
 
-    ! The ELRA model grid - I think this is not needed, ELRA use the same square grid from GIA
-    !TYPE(type_grid)                         :: grid 
-    
-    REAL(dp), DIMENSION(:    ), ALLOCATABLE :: surface_load_mesh                ! [Pa] Total surface load applied at each mesh point
-    REAL(dp), DIMENSION(:    ), ALLOCATABLE :: dHb_grid_partial                 ! [m]  Partial bedrock displacement on the grid
-    REAL(dp), DIMENSION(: , :), ALLOCATABLE :: dHb_grid_tot                     ! [m]  Total bedrock displacement on the grid
-    REAL(dp), DIMENSION(: , :), ALLOCATABLE :: dHb_eq_grid                      ! [m]  Bedrock displacement at equilibrium (computed from relative surface load)
-    REAL(dp), DIMENSION(: , :), ALLOCATABLE :: dHb_dt_grid                      ! [m/yr] Bedrock deformation rate on the grid
-    REAL(dp), DIMENSION(:    ), ALLOCATABLE :: dHb_dt_grid_partial              ! [m/yr] Partial bedrock deformation rate on the grid
-    REAL(dp), DIMENSION(:    ), ALLOCATABLE :: dHb_dt_mesh                      ! [m/yr] Bedrock deformation rate mapped on the mesh
-    REAL(dp), DIMENSION(: , :), ALLOCATABLE :: relative_surface_load_grid_tot   ! [Pa] Relative surface load (difference from equilibrium) on the full grid
-    REAL(dp), DIMENSION(:    ), ALLOCATABLE :: surface_load_GIAeq               ! [Pa] Reference surface load used for GIA equilibrium computation
-    integer                                 :: flex_prof_rad                    ! radius of the flexoral profile
-    REAL(dp), DIMENSION(: , :), ALLOCATABLE :: flex_prof_grid                   ! radius of the flexoral profile on GIA grid    
-    
-  END TYPE type_ELRA_model
- 
-CONTAINS
+    real(dp), dimension(:  ), allocatable :: surface_load_mesh                ! [Pa]   Total surface load applied at each mesh point
+    real(dp), dimension(:  ), allocatable :: dHb_grid_partial                 ! [m]    Partial bedrock displacement on the grid
+    real(dp), dimension(:,:), allocatable :: dHb_grid_tot                     ! [m]    Total bedrock displacement on the grid
+    real(dp), dimension(:,:), allocatable :: dHb_eq_grid                      ! [m]    Bedrock displacement at equilibrium (computed from relative surface load)
+    real(dp), dimension(:,:), allocatable :: dHb_dt_grid                      ! [m/yr] Bedrock deformation rate on the grid
+    real(dp), dimension(:  ), allocatable :: dHb_dt_grid_partial              ! [m/yr] Partial bedrock deformation rate on the grid
+    real(dp), dimension(:  ), allocatable :: dHb_dt_mesh                      ! [m/yr] Bedrock deformation rate mapped on the mesh
+    real(dp), dimension(:,:), allocatable :: relative_surface_load_grid_tot   ! [Pa]   Relative surface load (difference from equilibrium) on the full grid
+    real(dp), dimension(:  ), allocatable :: surface_load_GIAeq               ! [Pa]   Reference surface load used for GIA equilibrium computation
+    integer                               :: flex_prof_rad                    ! radius of the flexoral profile
+    real(dp), dimension(:,:), allocatable :: flex_prof_grid                   ! radius of the flexoral profile on GIA grid
 
-END MODULE GIA_model_types
+  end type type_ELRA_model
+
+end module GIA_model_types

ea9bb96605df42c5ed52983739d8cdf1f46f941b
TijnBerends <c.j.berends@uu.nl>
2026-08-28 21:07:09 +0200

    Convert old IMAU-ICE code

--- src/UPSY/mesh/remapping/remapping_grid_to_grid.f90
+++ src/UPSY/mesh/remapping/remapping_grid_to_grid.f90
@@ -5,8 +5,8 @@ module remapping_grid_to_grid
   use call_stack_and_comp_time_tracking, only: init_routine, finalise_routine
   use crash_mod, only: crash
   use grid_types, only: type_grid
-  use CSR_matrix_mod, only: type_CSR_matrix_dp
-  use remapping_types, only: type_map
+  use mpi_distributed_memory_grid, only: gather_gridded_data_to_primary, distribute_gridded_data_from_primary
+  use line_integrals, only: line_integral_mxydx, line_integral_xydy
 
   implicit none
 
@@ -26,266 +26,274 @@ contains
     real(dp), dimension(grid_dst%pai%i1:grid_dst%pai%i2), intent(  out) :: d_grid_dst_vec_partial
 
     ! Local variables:
-    character(len=*), parameter :: routine_name = 'map_from_xy_grid_to_xy_grid_2D'
+    character(len=*), parameter           :: routine_name = 'map_from_xy_grid_to_xy_grid_2D'
+    real(dp), dimension(:,:), allocatable :: d_grid_src
+    real(dp), dimension(:,:), allocatable :: d_grid_dst
+    real(dp)                              :: Ad
+    integer,  dimension(grid_dst%nx)      :: il_src, iu_src
+    integer,  dimension(grid_dst%ny)      :: jl_src, ju_src
+    integer                               :: i_src, j_src, i_dst, j_dst, i,j
+    real(dp), dimension(:,:), allocatable :: ddx_grid_src
+    real(dp), dimension(:,:), allocatable :: ddy_grid_src
+    real(dp)                              :: Asum, Asd, w0, w1x, w1y
+    logical,  dimension(:,:), allocatable :: mask_dst_outside_src
+    real(dp)                              :: xomin, xomax, yomin, yomax
+    real(dp), dimension(2)                :: sw, se, nw, ne
+    integer                               :: igmin, igmax, jgmin, jgmax
 
     ! Add routine to path
     call init_routine( routine_name)
 
-    ! ! Allocate shared memory
-    ! CALL allocate_shared_dp_2D(  ny_src, nx_src, ddx_src,              wddx_src             )
-    ! CALL allocate_shared_dp_2D(  ny_src, nx_src, ddy_src,              wddy_src             )
-    ! CALL allocate_shared_int_2D( ny_dst, nx_dst, mask_dst_outside_src, wmask_dst_outside_src)
+    ! Let the primary do the work
 
-    ! ! Find grid spacings
-    ! dx_src = x_src(2) - x_src(1)
-    ! dy_src = y_src(2) - y_src(1)
-    ! dx_dst = x_dst(2) - x_dst(1)
-    ! dy_dst = y_dst(2) - y_dst(1)
-    ! Ad = dx_dst * dy_dst
+    ! NOTE: not very elegant or efficient, but right now this code only has one call site,
+    ! being the remapping of the GIA-ELRA spatially variable relaxation time from its
+    ! NetCDF input grid to the GIA grid. That only happens during model initialisation, and
+    ! involves two coarse-resolution grids, so it won't noticeably slow down the model.
+    ! And this code will be replaced with the remapping overhaul that is planned for 2027 anyway...
 
-    ! ! If the grids are equal, the solution is trivial; just copy the data
-    ! IF (dx_src == dx_dst .AND. dy_src == dy_dst .AND. nx_src == nx_dst .AND. ny_src == ny_dst) THEN
-    !   CALL partition_list( nx_dst, par%i, par%n, i1, i2)
-    !   d_dst( :,i1:i2) = d_src( :,i1:i2)
-    !   CALL sync
-    !   CALL deallocate_shared(  wddx_src             )
-    !   CALL deallocate_shared(  wddy_src             )
-    !   CALL deallocate_shared(  wmask_dst_outside_src)
-    !   CALL finalise_routine( routine_name)
-    !   RETURN
-    ! END IF
+    if (par%primary) then
+      allocate( d_grid_src( grid_src%nx, grid_src%ny), source = 0._dp)
+      allocate( d_grid_src( grid_src%nx, grid_src%ny), source = 0._dp)
+    else
+      allocate( d_grid_src( 0,0))
+      allocate( d_grid_src( 0,0))
+    end if
+    call gather_gridded_data_to_primary( grid_src, d_grid_src_vec_partial, d_grid_src)
 
-    ! ! Find overlaps between grids
-    ! ! dst cell [i,j] overlaps with src cells [il_src( i):iu_src( i), jl_src( j):ju_src( j)]
+    if (par%primary) then
 
-    ! ! il
-    ! DO i_dst = 1, nx_dst
+      Ad = grid_dst%dx**2
 
-    !   IF (i_dst == 1) THEN
-    !     il_src( i_dst) = 1
-    !   ELSE
-    !     il_src( i_dst) = il_src( i_dst-1)
-    !   END IF
+    ! Find overlaps between grids
+    ! dst cell [i,j] overlaps with src cells [il_src( i):iu_src( i), jl_src( j):ju_src( j)]
 
-    !   DO WHILE (x_src( il_src( i_dst)) + dx_src / 2._dp < x_dst( i_dst) - dx_dst / 2._dp)
-    !     il_src( i_dst) = il_src( i_dst) + 1
-    !     IF (il_src( i_dst) >= nx_src) THEN
-    !       EXIT
-    !     END IF
-    !   END DO
-    !   il_src( i_dst) = MAX( 1, MIN( nx_src, il_src( i_dst) ))
+    ! il
+    do i_dst = 1, grid_dst%nx
 
-    ! END DO ! DO i_dst = 1, nx_dst
+      if (i_dst == 1) then
+        il_src( i_dst) = 1
+      else
+        il_src( i_dst) = il_src( i_dst-1)
+      end if
 
-    ! il_src = il_src - 1
-    ! il_src = MAX( 1, MIN( nx_src, il_src))
+      do while (grid_src%x( il_src( i_dst)) + grid_src%dx / 2._dp < grid_dst%x( i_dst) - grid_dst%dx / 2._dp)
+        il_src( i_dst) = il_src( i_dst) + 1
+        if (il_src( i_dst) >= grid_src%nx) then
+          exit
+        end if
+      end do
+      il_src( i_dst) = max( 1, min( grid_src%nx, il_src( i_dst) ))
 
-    ! ! iu
-    ! DO i_dst = nx_dst, 1, -1
+    end do
 
-    !   IF (i_dst == nx_dst) THEN
-    !     iu_src( i_dst) = nx_src
-    !   ELSE
-    !     iu_src( i_dst) = il_src( i_dst+1)
-    !   END IF
+    il_src = il_src - 1
+    il_src = max( 1, min( grid_src%nx, il_src))
 
-    !   DO WHILE (x_src( iu_src( i_dst)) - dx_src / 2._dp > x_dst( i_dst) + dx_dst / 2._dp)
-    !     iu_src( i_dst) = iu_src( i_dst) - 1
-    !     IF (iu_src( i_dst) <= 1) THEN
-    !       EXIT
-    !     END IF
-    !   END DO
-    !   iu_src( i_dst) = MAX( 1, MIN( nx_src, iu_src( i_dst) ))
+    ! iu
+    do i_dst = grid_dst%nx, 1, -1
 
-    ! END DO ! DO i_dst = nx_dst, 1, -1
+      if (i_dst == grid_dst%nx) then
+        iu_src( i_dst) = grid_src%nx
+      else
+        iu_src( i_dst) = il_src( i_dst+1)
+      end if
 
-    ! iu_src = iu_src + 1
-    ! iu_src = MAX( 1, MIN( nx_src, iu_src))
+      do while (grid_src%x( iu_src( i_dst)) - grid_src%dx / 2._dp > grid_dst%x( i_dst) + grid_dst%dx / 2._dp)
+        iu_src( i_dst) = iu_src( i_dst) - 1
+        if (iu_src( i_dst) <= 1) then
+          exit
+        end if
+      end do
+      iu_src( i_dst) = max( 1, min( grid_src%nx, iu_src( i_dst) ))
 
-    ! ! jl
-    ! DO j_dst = 1, ny_dst
+    end do
 
-    !   IF (j_dst == 1) THEN
-    !     jl_src( j_dst) = 1
-    !   ELSE
-    !     jl_src( j_dst) = jl_src( j_dst-1)
-    !   END IF
+    iu_src = iu_src + 1
+    iu_src = max( 1, min( grid_src%nx, iu_src))
 
-    !   DO WHILE (y_src( jl_src( j_dst)) + dx_src / 2._dp < y_dst( j_dst) - dx_dst / 2._dp)
-    !     jl_src( j_dst) = jl_src( j_dst) + 1
-    !     IF (jl_src( j_dst) >= ny_src) THEN
-    !       EXIT
-    !     END IF
-    !   END DO
-    !   jl_src( j_dst) = MAX( 1, MIN( ny_src, jl_src( j_dst) ))
+    ! jl
+    do j_dst = 1, grid_dst%ny
 
-    ! END DO ! DO j_dst = 1, ny_dst
+      if (j_dst == 1) then
+        jl_src( j_dst) = 1
+      else
+        jl_src( j_dst) = jl_src( j_dst-1)
+      end if
 
-    ! jl_src = jl_src - 1
-    ! jl_src = MAX( 1, MIN( ny_src, jl_src))
+      do while (grid_src%y( jl_src( j_dst)) + grid_src%dx / 2._dp < grid_dst%y( j_dst) - grid_dst%dx / 2._dp)
+        jl_src( j_dst) = jl_src( j_dst) + 1
+        if (jl_src( j_dst) >= grid_src%ny) then
+          exit
+        end if
+      end do
+      jl_src( j_dst) = max( 1, min( grid_src%ny, jl_src( j_dst) ))
 
-    ! ! ju
-    ! DO j_dst = ny_dst, 1, -1
+    end do
 
-    !   IF (j_dst == ny_dst) THEN
-    !     ju_src( j_dst) = ny_src
-    !   ELSE
-    !     ju_src( j_dst) = jl_src( j_dst+1)
-    !   END IF
+    jl_src = jl_src - 1
+    jl_src = max( 1, min( grid_src%ny, jl_src))
 
-    !   DO WHILE (y_src( ju_src( j_dst)) - dx_src / 2._dp > y_dst( j_dst) + dx_dst / 2._dp)
-    !     ju_src( j_dst) = ju_src( j_dst) - 1
-    !     IF (ju_src( j_dst) <= 1) THEN
-    !       EXIT
-    !     END IF
-    !   END DO
-    !   ju_src( j_dst) = MAX( 1, MIN( ny_src, ju_src( j_dst) ))
+    ! ju
+    do j_dst = grid_dst%ny, 1, -1
 
-    ! END DO ! DO j_dst = ny_dst, 1, -1
+      if (j_dst == grid_dst%ny) then
+        ju_src( j_dst) = grid_src%ny
+      else
+        ju_src( j_dst) = jl_src( j_dst+1)
+      end if
 
-    ! ju_src = ju_src + 1
-    ! ju_src = MAX( 1, MIN( ny_src, ju_src))
+      do while (grid_src%y( ju_src( j_dst)) - grid_src%dx / 2._dp > grid_dst%y( j_dst) + grid_dst%dx / 2._dp)
+        ju_src( j_dst) = ju_src( j_dst) - 1
+        if (ju_src( j_dst) <= 1) then
+          exit
+        end if
+      end do
+      ju_src( j_dst) = max( 1, min( grid_src%ny, ju_src( j_dst) ))
 
-    ! ! Get derivatives of d_src
-    ! CALL partition_list( nx_src, par%i, par%n, i1, i2)
-    ! DO i = MAX(2,i1), MIN(nx_src-1,i2)
-    ! DO j = 2, ny_src-1
-    !   ddx_src( j,i) = (d_src( j,i+1) - d_src( j,i-1)) / (2._dp * dx_src)
-    !   ddy_src( j,i) = (d_src( j+1,i) - d_src( j-1,i)) / (2._dp * dy_src)
-    ! END DO
-    ! END DO
-    ! CALL sync
+    end do
 
-    ! ! Find parallelisation domains
-    ! CALL partition_list( nx_dst, par%i, par%n, i1, i2)
-    ! CALL partition_list( ny_dst, par%i, par%n, j1, j2)
+    ju_src = ju_src + 1
+    ju_src = max( 1, min( grid_src%ny, ju_src))
 
-    ! DO i = i1, i2
-    ! DO j = 1, ny_dst
+    ! Calculate derivatives of d_src
+    allocate( ddx_grid_src( grid_src%nx, grid_src%ny), source = 0._dp)
+    allocate( ddy_grid_src( grid_src%nx, grid_src%ny), source = 0._dp)
+    do i_src = 2, grid_src%nx-1
+    do j_src = 2, grid_src%ny-1
+      ddx_grid_src( i_src,j_src) = (d_grid_src( i_src+1,j_src  ) - d_grid_src( i_src-1,j_src  )) / (2._dp * grid_src%dx)
+      ddy_grid_src( i_src,j_src) = (d_grid_src( i_src  ,j_src+1) - d_grid_src( i_src  ,j_src-1)) / (2._dp * grid_src%dx)
+    end do
+    end do
 
-    !   d_dst( j,i) = 0._dp
-    !   Asum        = 0._dp
+    ! Perform 2nd-order conservative remapping
+    allocate( mask_dst_outside_src( grid_dst%nx, grid_dst%ny), source = .false.)
+    do i = 1, grid_dst%nx
+    do j = 1, grid_dst%ny
 
-    !   ! If this dst cell lies (partly) outside of the src grid, mark it as such;
-    !   ! in that case, use nearest-neighbour extrapolation instead of conservative remapping
-    !   IF (x_dst( i) - dx_dst/2._dp < MINVAL( x_src) - dx_src/2._dp .OR. &
-    !       x_dst( i) + dx_dst/2._dp > MAXVAL( x_src) + dx_src/2._dp .OR. &
-    !       y_dst( j) - dx_dst/2._dp < MINVAL( y_src) - dx_src/2._dp .OR. &
-    !       y_dst( j) + dx_dst/2._dp > MAXVAL( y_src) + dx_src/2._dp) THEN
-    !     mask_dst_outside_src( j,i) = 1
-    !     CYCLE
-    !   ELSE
-    !     mask_dst_outside_src( j,i) = 0
-    !   END IF
+      d_grid_dst( i,j) = 0._dp
+      Asum             = 0._dp
 
-    !   DO i_src = il_src( i), iu_src( i)
-    !   DO j_src = jl_src( j), ju_src( j)
+      ! If this dst cell lies (partly) outside of the src grid, mark it as such;
+      ! in that case, use nearest-neighbour extrapolation instead of conservative remapping
+      if (grid_dst%x( i) - grid_dst%dx/2._dp < minval( grid_src%x) - grid_src%dx/2._dp .or. &
+          grid_dst%x( i) + grid_dst%dx/2._dp > maxval( grid_src%x) + grid_src%dx/2._dp .or. &
+          grid_dst%y( j) - grid_dst%dx/2._dp < minval( grid_src%y) - grid_src%dx/2._dp .or. &
+          grid_dst%y( j) + grid_dst%dx/2._dp > maxval( grid_src%y) + grid_src%dx/2._dp) then
+        mask_dst_outside_src( i,j) = .true.
+        cycle
+      end if
 
-    !     xomin = MAX( x_dst( i) - dx_dst/2._dp, x_src( i_src) - dx_src/2._dp)
-    !     xomax = MIN( x_dst( i) + dx_dst/2._dp, x_src( i_src) + dx_src/2._dp)
-    !     yomin = MAX( y_dst( j) - dy_dst/2._dp, y_src( j_src) - dy_src/2._dp)
-    !     yomax = MIN( y_dst( j) + dy_dst/2._dp, y_src( j_src) + dy_src/2._dp)
+      do i_src = il_src( i), iu_src( i)
+      do j_src = jl_src( j), ju_src( j)
 
-    !     IF (xomax <= xomin .OR. yomax <= yomin) CYCLE
+        xomin = max( grid_dst%x( i) - grid_dst%dx/2._dp, grid_src%x( i_src) - grid_src%dx/2._dp)
+        xomax = min( grid_dst%x( i) + grid_dst%dx/2._dp, grid_src%x( i_src) + grid_src%dx/2._dp)
+        yomin = max( grid_dst%y( j) - grid_dst%dx/2._dp, grid_src%y( j_src) - grid_src%dx/2._dp)
+        yomax = min( grid_dst%y( j) + grid_dst%dx/2._dp, grid_src%y( j_src) + grid_src%dx/2._dp)
 
-    !     Asd  = (xomax - xomin) * (yomax - yomin)
-    !     Asum = Asum + Asd
+        if (xomax <= xomin .or. yomax <= yomin) cycle
 
-    !     w0  = Asd / Ad
-    !     w1x = 1._dp / Ad * (line_integral_mxydx( [xomin,yomin], [xomax,yomin], 1E-9_dp) + &
-    !                         line_integral_mxydx( [xomax,yomin], [xomax,yomax], 1E-9_dp) + &
-    !                         line_integral_mxydx( [xomax,yomax], [xomin,yomax], 1E-9_dp) + &
-    !                         line_integral_mxydx( [xomin,yomax], [xomin,yomin], 1E-9_dp)) - w0 * x_src( i_src)
-    !     w1y = 1._dp / Ad * (line_integral_xydy(  [xomin,yomin], [xomax,yomin], 1E-9_dp) + &
-    !                         line_integral_xydy(  [xomax,yomin], [xomax,yomax], 1E-9_dp) + &
-    !                         line_integral_xydy(  [xomax,yomax], [xomin,yomax], 1E-9_dp) + &
-    !                         line_integral_xydy(  [xomin,yomax], [xomin,yomin], 1E-9_dp)) - w0 * y_src( j_src)
+        Asd  = (xomax - xomin) * (yomax - yomin)
+        Asum = Asum + Asd
 
-    !     d_dst( j,i) = d_dst( j,i) + w0  * d_src(   j_src,i_src) + &
-    !                                 w1x * ddx_src( j_src,i_src) + &
-    !                                 w1y * ddy_src( j_src,i_src)
+        sw = [xomin, yomin]
+        se = [xomax, yomin]
+        nw = [xomin, yomax]
+        ne = [xomax, yomax]
 
-    !   END DO ! DO j_src = jr_src( j,1), jr_src( j,2)
-    !   END DO ! DO i_src = ir_src( i,1), ir_src( i,2)
+        w0  = Asd / Ad
 
-    !   ! Safety
-    !   IF (ABS( 1._dp - Asum / Ad) > 1E-4_dp) THEN
-    !     CALL crash('dst grid cell [{int_01},{int_02}] couldnt be completely filled! Asum = {dp_01}, Ad = {dp_02}', int_01 = j, int_02 = i, dp_01 = Asum, dp_02 = Ad)
-    !   END IF
+        w1x = 1._dp / Ad * (line_integral_mxydx( sw, se, 1E-9_dp) + &
+                            line_integral_mxydx( se, ne, 1E-9_dp) + &
+                            line_integral_mxydx( ne, nw, 1E-9_dp) + &
+                            line_integral_mxydx( nw, sw, 1E-9_dp)) - w0 * grid_src%x( i_src)
 
-    ! END DO ! DO j = 1, ny_dst
-    ! END DO ! DO i = i1, i2
-    ! CALL sync
+        w1y = 1._dp / Ad * (line_integral_xydy(  sw, se, 1E-9_dp) + &
+                            line_integral_xydy(  se, ne, 1E-9_dp) + &
+                            line_integral_xydy(  ne, nw, 1E-9_dp) + &
+                            line_integral_xydy(  nw, sw, 1E-9_dp)) - w0 * grid_src%y( j_src)
 
-    ! ! Use nearest-neighbour extrapolation for dst cells outside of the src grid
-    ! ! =========================================================================
+        d_grid_dst( i,j) = d_grid_dst( i,j) + &
+          w0  * d_grid_src  ( i_src, j_src) + &
+          w1x * ddx_grid_src( i_src, j_src) + &
+          w1y * ddy_grid_src( i_src, j_src)
 
-    ! ! Find the range of grid cells that were mapped correctly
-    ! igmin = 0
-    ! igmax = 0
-    ! jgmin = 0
-    ! jgmax = 0
+      end do
+      end do
 
-    ! j = INT( REAL(ny_dst,dp)/2._dp)
-    ! DO i = 1, nx_dst
-    !   IF (mask_dst_outside_src( j,i) == 0) THEN
-    !     igmin = i
-    !     EXIT
-    !   END IF
-    ! END DO
-    ! DO i = nx_dst, 1, -1
-    !   IF (mask_dst_outside_src( j,i) == 0) THEN
-    !     igmax = i
-    !     EXIT
-    !   END IF
-    ! END DO
+      ! Safety
+      if (abs( 1._dp - Asum / Ad) > 1E-4_dp) then
+        call crash('dst grid cell [{int_01},{int_02}] couldnt be completely filled! Asum = {dp_01}, Ad = {dp_02}', int_01 = j, int_02 = i, dp_01 = Asum, dp_02 = Ad)
+      end if
 
-    ! i = INT( REAL(nx_dst,dp)/2._dp)
-    ! DO j = 1, ny_dst
-    !   IF (mask_dst_outside_src( j,i) == 0) THEN
-    !     jgmin = j
-    !     EXIT
-    !   END IF
-    ! END DO
-    ! DO j = ny_dst, 1, -1
-    !   IF (mask_dst_outside_src( j,i) == 0) THEN
-    !     jgmax = j
-    !     EXIT
-    !   END IF
-    ! END DO
+    end do
+    end do
 
-    ! ! Corners
-    ! IF (par%master) THEN
-    !   ! Southwest
-    !   d_dst( 1      :jgmin-1 ,1      :igmin-1) = d_dst( jgmin,igmin)
-    !   ! Southeast
-    !   d_dst( 1      :jgmin-1 ,igmax+1:nx_dst ) = d_dst( jgmin,igmax)
-    !   ! Northwest
-    !   d_dst( jgmax+1:ny_dst  ,1      :igmin-1) = d_dst( jgmax,igmin)
-    !   ! Northeast
-    !   d_dst( jgmax+1:ny_dst  ,igmax+1:nx_dst ) = d_dst( jgmax,igmax)
-    ! END IF ! IF (par%master) THEN
-    ! CALL sync
+    ! Use nearest-neighbour extrapolation for dst cells outside of the src grid
+    ! =========================================================================
 
-    ! ! Borders
-    ! DO i = MAX(i1,igmin), MIN(i2,igmax)
-    !   ! South
-    !   d_dst( 1      :jgmin-1,i) = d_dst( jgmin,i)
-    !   ! North
-    !   d_dst( jgmax+1:ny_dst ,i) = d_dst( jgmax,i)
-    ! END DO
-    ! DO j = MAX(j1,jgmin), MIN(j2,jgmax)
-    !   ! West
-    !   d_dst( j,1      :igmin-1) = d_dst( j,igmin)
-    !   ! East
-    !   d_dst( j,igmax+1:nx_dst ) = d_dst( j,igmax)
-    ! END DO
-    ! CALL sync
+    ! Find the range of grid cells that were mapped correctly
+    igmin = 0
+    igmax = 0
+    jgmin = 0
+    jgmax = 0
 
-    ! ! Clean up after yourself
-    ! CALL deallocate_shared( wddx_src             )
-    ! CALL deallocate_shared( wddy_src             )
-    ! CALL deallocate_shared( wmask_dst_outside_src)
+    j = int( real( grid_dst%ny,dp)/2._dp)
+    do i = 1, grid_dst%nx
+      if (.not. mask_dst_outside_src( i,j)) then
+        igmin = i
+        exit
+      end if
+    end do
+    do i = grid_dst%nx, 1, -1
+      if (.not. mask_dst_outside_src( i,j)) then
+        igmax = i
+        exit
+      end if
+    end do
+
+    i = int( real( grid_dst%nx,dp)/2._dp)
+    do j = 1, grid_dst%ny
+      if (.not. mask_dst_outside_src( i,j)) then
+        jgmin = j
+        exit
+      end if
+    end do
+    do j = grid_dst%ny, 1, -1
+      if (.not. mask_dst_outside_src( i,j)) then
+        jgmax = j
+        exit
+      end if
+    end do
+
+    ! Corners
+    ! Southwest
+    d_grid_dst( 1:igmin-1, 1:jgmin-1) = d_grid_dst( igmin, jgmin)
+    ! Southeast
+    d_grid_dst( igmax+1:grid_dst%nx, 1:jgmin-1) = d_grid_dst( igmax, jgmin)
+    ! Northwest
+    d_grid_dst( 1:igmin-1, jgmax+1:grid_dst%ny) = d_grid_dst( igmin, jgmax)
+    ! Northeast
+    d_grid_dst( igmax+1:grid_dst%nx, jgmax+1:grid_dst%ny) = d_grid_dst( igmax, jgmax)
+
+    ! Borders
+    do i = max( 1,igmin), min( grid_dst%nx,igmax)
+      ! South
+      d_grid_dst( i, 1:jgmin-1) = d_grid_dst( i, jgmin)
+      ! North
+      d_grid_dst( i, jgmax+1:grid_dst%ny) = d_grid_dst( i, jgmax)
+    end do
+    do j = max( 1,jgmin), min( grid_dst%ny,jgmax)
+      ! West
+      d_grid_dst( 1:igmin-1, j) = d_grid_dst( igmin, j)
+      ! East
+      d_grid_dst( igmax+1:grid_dst%nx, j) = d_grid_dst( igmax, j)
+    end do
+
+    end if ! if (par%primary) then
+
+    call distribute_gridded_data_from_primary( grid_dst, d_grid_dst_vec_partial, d_grid_dst)
 
     ! Finalise routine path
     call finalise_routine( routine_name)

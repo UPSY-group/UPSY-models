45a65937c4a865ade3619c3b323b460de5f707a4
TijnBerends <c.j.berends@uu.nl>
2026-09-01 14:54:20 +0200

    Add mesh_to_dmplex and component test
    YAY

--- automated_testing/UPSY/component_test_PETSc_DMPLEX/test_script.csh
+++ automated_testing/UPSY/component_test_PETSc_DMPLEX/test_script.csh
@@ -4,6 +4,7 @@ set test_dir = automated_testing/UPSY/component_test_PETSc_DMPLEX
 
 rm -rf ${test_dir}/results*
 
-mpiexec  -n 2  build/src/UPSY/UPSY_component_test_program_PETSc_DMPLEX  ${test_dir}/results  automated_testing/UPSY/component_test_PETSc_DMPLEX/test_matrix_equations
+mpiexec  -n 2  build/src/UPSY/UPSY_component_test_program_PETSc_DMPLEX  automated_testing/test_meshes_and_grids  ${test_dir}/results
+find ${test_dir}/results -type f -name '*.h5' -exec sh -c 'python3 "$1" "$2" --output "${2%.h5}.png"' sh ${test_dir}/visualise_DMPLEX.py {} \;
 
-python3 automated_testing/reduce_all_netcdfs_in_folder_to_checksum.py ${test_dir}
+
--- src/UPSY/validation/component_tests/PETSc_finite_elements/ct_PETSc_DMPLEX_basics.f90
+++ src/UPSY/validation/component_tests/PETSc_finite_elements/ct_PETSc_DMPLEX_basics.f90
@@ -4,19 +4,28 @@ module ct_PETSc_DMPLEX_basics
   use precisions, only: dp
   use call_stack_and_comp_time_tracking, only: init_routine, finalise_routine
   use crash_mod, only: warning
+  use mpi_basic, only: par
+  use mesh_types, only: type_mesh
+  use netcdf_io_main, only: open_existing_netcdf_file_for_reading, setup_mesh_from_file, &
+    close_netcdf_file
   use petsc, only: PetscErrorF, tDM, tVec, tPetscViewer, tPetscSection, DMPlexCreate, DMSetDimension, &
     DMSetCoordinateDim, DMPlexSetChart, DMPlexSetConeSize, DMSetUp, DMPlexSetCone, &
     DMPlexSymmetrize, DMPlexStratify, DMGetCoordinateSection, DMGetCoordinateDM, &
     DMCreateLocalVector, DMSetCoordinatesLocal, PetscSectionSetChart, PetscSectionSetDof, &
     PetscSectionSetUp, VecSetValues, VecAssemblyBegin, VecAssemblyEnd, INSERT_VALUES, &
     VecDestroy, PETSC_COMM_WORLD, DMDestroy, PetscObjectSetName
-  use petsc_basic, only: write_dmplex_to_hdf5
+  use petsc_dmplex, only: write_dmplex_to_hdf5, mesh_to_dmplex
+  use mesh_memory, only: allocate_mesh_primary
+  use mesh_dummy_meshes, only: initialise_dummy_mesh_5, initialise_dummy_mesh_9, &
+    initialise_dummy_mesh_16
+  use mesh_edges, only: construct_mesh_edges
+  use string_module, only: strrep
 
   implicit none
 
   private
 
-  public :: create_simple_DMPLEX
+  public :: create_simple_DMPLEX, ct_convert_meshes_to_dmplex
 
 contains
 
@@ -63,6 +72,9 @@ contains
 
     ! Add routine to call stack
     call init_routine( routine_name)
+
+    if (par%primary) write(0,*) '   Running PETSc DMPLEX test: create a super simple DMPLEX object and write it to an HDF5 file'
+    if (par%primary) write(0,*) ''
 
     ! Create a DMPlex object
     PetscCall( DMPlexCreate( PETSC_COMM_WORLD, dm, ierr))
@@ -158,5 +170,98 @@ contains
     call finalise_routine( routine_name)
 
   end subroutine create_simple_DMPLEX
+
+  subroutine ct_convert_meshes_to_dmplex( foldername_output, test_mesh_filenames, test_grid_filenames)
+
+    ! In/output variables:
+    character(len=*),               intent(in) :: foldername_output
+    character(len=*), dimension(:), intent(in) :: test_mesh_filenames
+    character(len=*), dimension(:), intent(in) :: test_grid_filenames
+
+    ! Local variables:
+    character(len=*), parameter   :: routine_name = 'ct_convert_meshes_to_dmplex'
+    integer                       :: i_mesh
+    type(type_mesh), allocatable  :: mesh
+    character(len=:), allocatable :: filename_mesh
+    integer                       :: ncid
+
+    ! Add routine to call stack
+    call init_routine( routine_name)
+
+    if (par%primary) write(0,*) '   Running PETSc DMPLEX test: converting UFEMISM meshes to DMPLEX objects'
+    if (par%primary) write(0,*) ''
+
+    ! Dummy meshes
+    allocate( mesh)
+    call allocate_mesh_primary( mesh, 'test_mesh_dummy_5', 100, 200)
+    call initialise_dummy_mesh_5( mesh, 0._dp, 1._dp, 0._dp, 1._dp)
+    call construct_mesh_edges( mesh)
+    call ct_convert_mesh_to_dmplex_and_write_to_hdf5( foldername_output, mesh)
+    deallocate( mesh)
+
+    allocate( mesh)
+    call allocate_mesh_primary( mesh, 'test_mesh_dummy_9', 100, 200)
+    call initialise_dummy_mesh_9( mesh, 0._dp, 1._dp, 0._dp, 1._dp)
+    call construct_mesh_edges( mesh)
+    call ct_convert_mesh_to_dmplex_and_write_to_hdf5( foldername_output, mesh)
+    deallocate( mesh)
+
+    allocate( mesh)
+    call allocate_mesh_primary( mesh, 'test_mesh_dummy_16', 100, 200)
+    call initialise_dummy_mesh_16( mesh, 0._dp, 1._dp, 0._dp, 1._dp)
+    call construct_mesh_edges( mesh)
+    call ct_convert_mesh_to_dmplex_and_write_to_hdf5( foldername_output, mesh)
+    deallocate( mesh)
+
+    ! Meshes read from files
+    do i_mesh = 1, size( test_mesh_filenames)
+      filename_mesh = test_mesh_filenames( i_mesh)
+      allocate( mesh)
+      call open_existing_netcdf_file_for_reading( filename_mesh, ncid)
+      call setup_mesh_from_file( filename_mesh, ncid, mesh)
+      call close_netcdf_file( ncid)
+      call ct_convert_mesh_to_dmplex_and_write_to_hdf5 ( foldername_output, mesh)
+      deallocate( mesh)
+    end do
+
+    ! Remove routine from call stack
+    call finalise_routine( routine_name)
+
+  end subroutine ct_convert_meshes_to_dmplex
+
+  subroutine ct_convert_mesh_to_dmplex_and_write_to_hdf5( foldername_output, mesh)
+
+    ! In/output variables:
+    character(len=*), intent(in) :: foldername_output
+    type(type_mesh),  intent(in) :: mesh
+
+    ! Local variables:
+    character(len=*), parameter   :: routine_name = 'ct_convert_mesh_to_dmplex_and_write_to_hdf5'
+    type(tDM)                     :: dm
+    integer                       :: ierr
+    character(len=:), allocatable :: mesh_name_cleaned
+    character(len=:), allocatable :: filename
+
+    ! Add routine to call stack
+    call init_routine( routine_name)
+
+    if (par%primary) write(0,*) '    Converting UFEMISM mesh ', trim( mesh%name), ' to DMPLEX object...'
+
+    call mesh_to_dmplex( mesh, dm)
+
+    mesh_name_cleaned = trim( mesh%name)
+    mesh_name_cleaned = strrep( mesh_name_cleaned, '"', '')
+    mesh_name_cleaned = strrep( mesh_name_cleaned, '.', '_')
+    mesh_name_cleaned = strrep( mesh_name_cleaned, '/', '_')
+    filename = trim( foldername_output) // '/' // trim( mesh_name_cleaned) // '_dmplex.h5'
+    call write_dmplex_to_hdf5( dm, filename)
+
+    ! Clean up after yourself
+    PetscCall( DMDestroy( dm, ierr))
+
+    ! Remove routine from call stack
+    call finalise_routine( routine_name)
+
+  end subroutine ct_convert_mesh_to_dmplex_and_write_to_hdf5
 
 end module ct_PETSc_DMPLEX_basics
